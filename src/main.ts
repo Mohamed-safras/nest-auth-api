efab8062b2d581b966efa056a7ef9a989c258fa9
Mohamed Safras <mohamed.safras@acentura.com>
2023-11-10 11:22:11 +0530

    swagger module has been added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cookieParser());
+  const config = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('HCAI Auth Service')
+    .setDescription('The Auth Service API description')
+    .setVersion('1.0')
+    .addTag('Authentication')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(config.get<number>('port'));
 }
 bootstrap();
